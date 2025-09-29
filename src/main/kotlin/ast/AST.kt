3ae3052fafa382a4fd2b16b72eb5c5e7693c2370
Aznos <91508926+Aznos@users.noreply.github.com>
2025-09-28 22:38:27 -0400

    Updated AST to include Spans

--- src/main/kotlin/ast/AST.kt
+++ src/main/kotlin/ast/AST.kt
@@ -1,87 +1,90 @@
 package ast
 
+import diagnostics.Span
 import kotlinx.serialization.Serializable
 
 @Serializable data class Program(val decls: List<Decl>)
 
-@Serializable sealed interface Decl
+@Serializable sealed interface Decl { val span: Span }
 @Serializable data class FnDecl(
     val name: String,
     val params: List<Param>,
     val returnType: TypeRef?,
-    val body: Block
+    val body: Block,
+    override val span: Span
 ) : Decl
 
-@Serializable data class Param(val name: String, val type: TypeRef)
-@Serializable data class Block(val stmts: List<Stmt>)
+@Serializable data class Param(val name: String, val type: TypeRef, val span: Span)
+@Serializable data class Block(val stmts: List<Stmt>, val span: Span)
 
-@Serializable sealed interface Stmt
+@Serializable sealed interface Stmt { val span: Span }
 @Serializable data class VarDecl(
     val name: String,
     val annotatedType: TypeRef?,
     val init: Expr?,
-    val isConst: Boolean
+    val isConst: Boolean,
+    override val span: Span
 ) : Stmt
 
 @Serializable data class Assign(
     val name: String,
-    val value: Expr
+    val value: Expr,
+    override val span: Span
 ) : Stmt
 
-@Serializable sealed interface Expr
-@Serializable data class ExprStmt(val expr: Expr) : Stmt
+@Serializable data class ExprStmt(val expr: Expr, override val span: Span) : Stmt
 
+@Serializable sealed interface Expr { val span: Span }
 @Serializable data class Binary(
     val op: BinOp,
     val left: Expr,
-    val right: Expr
+    val right: Expr,
+    override val span: Span
 ) : Expr
 
-@Serializable enum class BinOp {
-    Add, Sub, Mul, Div, Mod, Pow,
-    Eq, Ne, Lt, Le, Gt, Ge,
-    And, Or
-}
+@Serializable enum class BinOp { Add, Sub, Mul, Div, Mod, Pow, Eq, Ne, Lt, Le, Gt, Ge, And, Or }
 
 @Serializable data class Call(
     val callee: String,
-    val args: List<Expr>
+    val args: List<Expr>,
+    override val span: Span
 ) : Expr
 
 @Serializable data class MethodCall(
     val receiver: Expr,
     val method: String,
-    val args: List<Expr>
+    val args: List<Expr>,
+    override val span: Span
 ) : Expr
 
 @Serializable data class IfStmt(
     val branches: List<IfBranch>,
-    val elseBranch: Block?
+    val elseBranch: Block?,
+    override val span: Span
 ) : Stmt
 
-@Serializable data class IfBranch(
-    val condition: Expr,
-    val body: Block
-)
+@Serializable data class IfBranch(val condition: Expr, val body: Block, val span: Span)
 
 @Serializable data class WhileStmt(
     val condition: Expr,
-    val body: Block
+    val body: Block,
+    override val span: Span
 ) : Stmt
 
 @Serializable data class Unary(
     val op: UnOp,
-    val expr: Expr
+    val expr: Expr,
+    override val span: Span
 ) : Expr
 
 @Serializable enum class UnOp { Not }
 
-@Serializable data class Ident(val name: String) : Expr
-@Serializable data class IntLit(val value: Long) : Expr
-@Serializable data class FloatLit(val value: Double) : Expr
-@Serializable data class BoolLit(val value: Boolean) : Expr
-@Serializable data class CharLit(val value: Int) : Expr
-@Serializable data class StringLit(val value: String) : Expr
+@Serializable data class Ident(val name: String, override val span: Span) : Expr
+@Serializable data class IntLit(val value: Long, override val span: Span) : Expr
+@Serializable data class FloatLit(val value: Double, override val span: Span) : Expr
+@Serializable data class BoolLit(val value: Boolean, override val span: Span) : Expr
+@Serializable data class CharLit(val value: Int, override val span: Span) : Expr
+@Serializable data class StringLit(val value: String, override val span: Span) : Expr
 
-@Serializable sealed interface TypeRef
-@Serializable data class NamedType(val name: String) : TypeRef
+@Serializable sealed interface TypeRef { val span: Span }
+@Serializable data class NamedType(val name: String, override val span: Span) : TypeRef
